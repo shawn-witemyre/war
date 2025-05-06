367d44846a54d8b37e198707957612e9b1dcd1da
Shawn Witemyre <switemyre@comcast.net>
2025-05-05 21:57:38 -0400

    Added html and api call

---
+++ index.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("button");
+
+button.addEventListener("click", function () {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+});
